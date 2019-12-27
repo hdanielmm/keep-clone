62b708e491ce3f9224b175255788f3b18e9a9315
Daniel <hdanielmm@gmail.com>
2019-12-27 00:50:18 -0500

    Add a simple visual effect

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -40,28 +46,39 @@ function CreateArea(props) {
       content: ""
     });
     e.preventDefault();
+    setIsExpanded(false);
+  }
+
+  function expandNote() {
+    setIsExpanded(true);
   }
 
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          value={note.title}
-          placeholder="Title"
-          onChange={handleChange}
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            value={note.title}
+            placeholder="Title"
+            onChange={handleChange}
+          />
+        )}
         <textarea
           name="content"
           value={note.content}
-          rows="3"
-          cols="30"
           placeholder="Take a note..."
           onChange={handleChange}
+          onClick={expandNote}
+          rows={isExpanded ? "3" : "1"}
         />
-        <button onClick={handleClick}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={handleClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
-    </div>
+    </div >
   )
 }
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
 
   function handleClick() {
     props.onDelete(props.id);
   }
-  
+
   return (
     <div className="note" key={props.key}>
-      <h1>{props.title}</h1>
+      <h1 contentEditable="true">{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   )
 }
